8ff19586c81c09d1842d76fd3bec7fa2e0880ca7
Juan Daniel <lie@MacBook-Pro.local>
2020-02-21 13:09:55 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const initialState = 0;
+  const [count, setCount] = useState(initialState)
+
+  const incrementFive = () => {
+    for (let index = 0; index < 5; index++) {
+        setCount (prevCount => prevCount + 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Count: {count}
+      <button onClick={() => {setCount(initialState)}}>Reset</button>
+      <button onClick={()=> setCount(count+1)}>Count</button>
+      <button onClick={incrementFive}>Increment</button>
     </div>
-  );
+  )
 }
+
 
 export default App;
