a0314df7d6187471798e5531e4d6f40c81c6dbae
Fauzanooe <91836349+Fauzanooe@users.noreply.github.com>
2021-10-03 22:54:57 +0700

    Create bila.php
    bila bot

---
+++ public/bila.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+
+<html class="js">
+
+  <head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+
+    <script
+
+      async
+
+      src="https://www.googletagmanager.com/gtag/js?id=G-4BGMDBHW9H"
+
+    ></script>
+
+    <script>
+
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+
+        dataLayer.push(arguments);
+
+      }
+
+      gtag("js", new Date());
+
+      gtag("config", "G-4BGMDBHW9H");
+
+    </script>
+
+    <meta charset="utf-8" />
+
+    <meta name="author" content="Softnio" />
+
+    <meta
+
+      name="viewport"
+
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+
+    />
+
+    <meta
+
+      name="description"
+
+      content="LedgerMail - World's first Decentralized Email Solution on a mission of protecting Digital Rights of email users in a Decentralized way! Highest level of Email Security, Privacy, Encryption and Prevention from email attacks with 4th generation Blockchain."
+
+    />
+
+    <link rel="shortcut icon" href="images/favicon.png" />
+
+    <title>
+
+      World's First Decentralized Email Solution | Blockchain based encrypted
+
+      and free email service
+
+    </title>
+
+    <!-- Vendor Bundle bila CSS -->
+
+    <link rel="stylesheet" href="assets/css/vendor.bundle.css?ver=142" />
+
+    <!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template -->
+
+    <!-- <link rel="stylesheet" href="assets/css/style.css?ver=142" /> -->
+
+    <link rel="stylesheet" href="assets/css/styleTwo.css?ver=142" />
+
+    <link rel="stylesheet" href="assets/css/theme-blue.css?ver=142" />
+
+    <link rel="stylesheet" href="assets/css/custom.css" />
+
+    <link rel="stylesheet" href="assets/css/pricing-table.css" />
+
+    <link rel="stylesheet" href="assets/css/slider.css" />
+
+    <link rel="stylesheet" href="assets/css/faq-section.css" />
+
+    <link rel="stylesheet" href="assets/css/intro-video.css" />
+
+    <link rel="stylesheet" href="assets/css/modal2.css" />
+
+    <link href="https://css.gg/play-button-o.css" rel="stylesheet" />
+
+    <!-- height: 70pxal site tag (gtag.js) - Google Analytics -->
+
+    <!-- text -->
+
+    <script
+
+      async
+
+      src="https://www.googletagmanager.com/gtag/js?id=UA-129657838-1"
+
+    ></script>
+
+    <script
+
+      src="https://www.google.com/recaptcha/api.js?onload=onLoad&render=explicit"
+
+      async
+
+      defer
+
+    ></script>
+
+    <script>
+
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+
+        dataLayer.push(arguments);
+
+      }
+
+      gtag("js", new Date());
+
+      gtag("config", "UA-129657838-1");
+
+    </script>
+
+    <!--Add the following script at the bottom of the web page (before </body></html>)-->
+
+    <script type="text/javascript">
+
+      function add_chatinline() {
+
+        var hccid = 26468622;
+
+        var nt = document.createElement("script");
+
+        nt.async = true;
+
+        nt.src = "https://mylivechat.com/chatinline.aspx?hccid=" + hccid;
+
+        var ct = document.getElementsByTagName("script")[0];
+
+        ct.parentNode.insertBefore(nt, ct);
+
+      }
+
+      add_chatinline();
+
+    </script>
+
+  </head>
+
+  <body
+
+    class="theme-dark io-lungwort"
+
+    data-spy="scroll"
+
+    data-target="#mainnav"
+
+    data-offset="80"
+
+  >
+
+    <!-- Navbar -->
+
+    <div id="coming-soon" class="team-profile mfp-hide">
+
+      <button title="Close (Esc)" type="button" class="mfp-close">×</button>
+
+      <div class="container-fluid">
+
+        <div class="row no-mg">
+
+          <div class="col-lg-9 text-center">
+
+            <div class="team-profile-info">
+
+              <h1 class="dark">Coming soon...</h1>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div id="pop-contact-form" class="pop-contact-form mfp-hide">
+
+      <button title="Close (Esc)" type="button" class="mfp-close">×</button>
+
+      <div class="container-fluid">
+
+        <div class="row no-mg">
+
+          <div class="col-lg-8 offset-lg-2">
+
+            <h3>Drop a message and we will connect with you.</h3>
+
+            <div class="gaps size-2x"></div>
+
+            <form
+
+              id="p-contact-form"
+
+              class="form-message form-message-s2 text-left"
+
+              action="https://tqobynyog0.execute-api.us-east-2.amazonaws.com/default/contact-form"
+
+              method="POST"
+
+              novalidate="novalidate"
+
+            >
+
+              <div class="form-results"></div>
+
+              <input type="hidden" name="dummy" value="dummy" />
+
+              <div
+
+                class="input-field animated fadeInUp"
+
+                data-animate="fadeInUp"
+
+                data-delay=".2"
+
+                style="visibility: visible; animation-delay: 0.7s"
+
+              >
+
+                <input
+
+                  name="name"
+
+                  type="text"
+
+                  placeholder="Your Name"
+
+                  class="input-border required"
+
+                  aria-required="true"
+
+                />
+
+              </div>
+
+              <input type="hidden" name="website" value="ledgermail-contact" />
+
+              <div
+
+                class="input-field animated fadeInUp"
+
+                data-animate="fadeInUp"
+
+                data-delay=".3"
+
+                style="visibility: visible; animation-delay: 0.8s"
+
+              >
+
+                <input
+
+                  name="email"
+
+                  type="email"
+
+                  placeholder="Your Email"
+
+                  class="input-border required email"
+
+                  aria-required="true"
+
+                />
+
+              </div>
+
+              <div
+
+                class="input-field animated fadeInUp"
+
+                data-animate="fadeInUp"
+
+                data-delay=".4"
+
+                style="visibility: visible; animation-delay: 0.9s"
+
+              >
+
+                <textarea
+
+                  name="message"
+
+                  placeholder="Your Message"
+
+                  class="txtarea input-border required"
+
+                  aria-required="true"
+
+                ></textarea>
+
+              </div>
+
+              <input
+
+                type="text"
+
+                class="d-none"
+
+                name="form-anti-honeypot"
+
+                value=""
+
+              />
+
+              <div
+
+                class="input-field animated fadeInUp"
+
+                data-animate="fadeInUp"
+
+                data-delay="0.5"
+
+                style="visibility: visible; animation-delay: 1s"
+
+              >
+
+                <div
+
+                  class="g-recaptcha"
+
+                  id="contact-pop-captcha"
+
+                  data-sitekey="6Lf5X5EUAAAAAOsnMMtfxZKXioIOuK1gGLf2vF8E"
+
+                  data-callback="onSubmitPop"
+
+                  data-size="invisible"
+
+                ></div>
+
+                <button type="submit" class="btn">Submit</button>
+
+              </div>
+
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
